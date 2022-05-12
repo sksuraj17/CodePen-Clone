69452b687e190a3b90014a9f92c40fe043688142
Suraj SK <surajsk2002@gmail.com>
2022-05-12 22:41:17 +0530

    Commit 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Editor from './components/Editor'
+
+
 const App = () => {
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+
   return (
     <>
     <div className='panel top-panel'>
-    <Editor title="HTML" language="xml" />
-    <Editor title="CSS" language="css" />
-    <Editor title="JS" language="javascript" />
+    <Editor title="HTML" language="xml" value={html} onChange={setHtml} />
+    <Editor title="CSS" language="css" value={css} onChange={setCss}/>
+    <Editor title="JS" language="javascript" value={js} onChange={setJs}/>
     </div>
     <div className='panel'>
 
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,14 +6,20 @@ import "codemirror/mode/css/css";
 import "codemirror/mode/javascript/javascript";
 import { Controlled as ControlledEditor } from 'react-codemirror2';
 
-const editor = ({language,title}) => {
+const editor = ({language,title,value,onChange}) => {
+    function handleChange(editor,data,value){
+        onChange(value)
+    }
   return (
       <div className='editor-container'>
         <div className='editor-title'>
             {title}
             <button>O/C</button>
         </div>
-        <ControlledEditor className='code-mirror-wrapper' options={{
+        <ControlledEditor
+        onBeforeChange={handleChange} 
+        value={value}
+            className='code-mirror-wrapper' options={{
             lineWrapping:true,
             lint:true,
             mode:language,
