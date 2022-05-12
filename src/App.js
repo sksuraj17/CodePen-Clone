e8c9198269dfcdc0765cc70504629b4cbc8044df
Suraj SK <surajsk2002@gmail.com>
2022-05-12 22:20:45 +0530

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import Editor from './components/Editor'
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <>
+    <div className='panel top-panel'>
+    <Editor />
+    <Editor />
+    <Editor />
     </div>
+    <div className='panel'>
+
+    </div>
+    </>
   )
 }
 
---
+++ src/components/Editor.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/css/css";
+import "codemirror/mode/javascript/javascript";
+import { Controlled as ControlledEditor } from 'react-codemirror2';
+
+const editor = ({language}) => {
+  return (
+      <div className='editor-container'>
+        <div className='editor-title'>
+            HTML
+            <button>O/C</button>
+        </div>
+        <ControlledEditor className='code-mirror-wrapper' options={{
+            lineWrapping:true,
+            lint:true,
+            mode:language,
+            theme:"material",
+            lineNumbers:true
+
+        }
+
+        }/>
+      </div>
+
+  )
+}
+
+export default editor
