4ea22487380441c0a1892d3c634cb45ea981c052
Suraj SK <surajsk2002@gmail.com>
2022-05-12 23:15:02 +0530

    Commit 6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React, { useState } from 'react'
-import Editor from './components/Editor'
-
+import React, { useEffect, useState } from "react";
+import Editor from "./components/Editor";
 
 const App = () => {
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+  <html>
+  <body>${html}</body>
+  <style>${css}</style>
+  <script>${js}</script>
+  </html>`);
+    }, 250);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [html, css, js]);
   return (
     <>
-    <div className='panel top-panel'>
-    <Editor title="HTML" language="xml" value={html} onChange={setHtml} />
-    <Editor title="CSS" language="css" value={css} onChange={setCss}/>
-    <Editor title="JS" language="javascript" value={js} onChange={setJs}/>
-    </div>
-    <div className='panel'>
-
-    </div>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameborder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,34 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
-import "codemirror/mode/css/css";
 import "codemirror/mode/javascript/javascript";
-import { Controlled as ControlledEditor } from 'react-codemirror2';
+import "codemirror/mode/css/css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import { Launch, TransitEnterexitSharp } from "@material-ui/icons";
 
-const editor = ({language,title,value,onChange}) => {
-    function handleChange(editor,data,value){
-        onChange(value)
-    }
+const Editor = (props) => {
+  const { language, displayName, value, onChange } = props;
+  const [open, setOpen] = useState(true);
+
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+
   return (
-      <div className='editor-container'>
-        <div className='editor-title'>
-            {title}
-            <button>O/C</button>
-        </div>
-        <ControlledEditor
-        onBeforeChange={handleChange} 
-        value={value}
-            className='code-mirror-wrapper' options={{
-            lineWrapping:true,
-            lint:true,
-            mode:language,
-            theme:"material",
-            lineNumbers:true
-        }}/>
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
+      <div className="editor-title">
+        {displayName}
+        <button
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+          className="expand-collapse-btn"
+        >
+          {open ? <TransitEnterexitSharp /> : <Launch />}
+        </button>
       </div>
-
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: "material",
+          lineNumbers: true,
+        }}
+      />
+    </div>
   );
 };
 
-export default editor
+export default Editor;
